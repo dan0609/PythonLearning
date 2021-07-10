d2a65094bd1f1eb48812e63eb55a7f34b75a5671
YinPengCheng <mikilangkilo.yin@ximalaya.com>
2021-07-10 18:53:48 +0800

    Question/计算总和

--- chapter_4/__init__.py
+++ chapter_4/__init__.py
@@ -1,17 +1,22 @@
-magicians = ['alice', 'david', 'carolina']
-
-# 打印magicians
+# Q1:打印magicians
+# magicians = ['alice', 'david', 'carolina']
 # for magician in magicians:
 #     print(magician)
 
-# 输出alice , david , carolina
+# Q2:输出alice , david , carolina
+# if __name__ == '__main__':
+#     magicians = ['alice', 'david', 'carolina']
+#     result = ""
+#     length = len(magicians)
+#     for index in range(0,length):
+#         if index != length - 1:
+#             result = magicians[index] + ' , '
+#         else:
+#             result = magicians[index]
+#     print(result)
 
+# Q3: 计算 input的总和
 if __name__ == '__main__':
-    result = ""
-    length = len(magicians)
-    for index in range(0,length):
-        if index != length - 1:
-            result = magicians[index] + ' , '
-        else:
-            result = magicians[index]
+    input = [1, 2, 3]
+    result = 0
     print(result)
