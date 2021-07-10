0e86fba7ea0192c0d0aea4fbeca13e9d7a4b5973
WuErZhen <13564301371@163.com>
2021-07-10 18:49:39 +0800

    feature/循环题目练习

--- chapter_4/__init__.py
+++ chapter_4/__init__.py
@@ -1,7 +1,17 @@
 magicians = ['alice', 'david', 'carolina']
 
 # 打印magicians
-for magician in magicians:
-    print(magician)
+# for magician in magicians:
+#     print(magician)
 
 # 输出alice , david , carolina
+
+if __name__ == '__main__':
+    result = ""
+    length = len(magicians)
+    for index in range(0,length):
+        if index != length - 1:
+            result = magicians[index] + ' , '
+        else:
+            result = magicians[index]
+    print(result)
